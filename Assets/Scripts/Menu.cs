c9d062daa09bd6d0deba8c4bedbae6ea0fa544bd
JamieC <jc228467@falmouth.ac.uk>
2021-01-29 23:23:42 +0000

    Implemented Start and Quit button functionality on Main Menu:
     - Created MainMenu Canvas.prefab
     - Removed SampleScene.unity
     - Created Main Level.unity
     - Added Main Menu.unity and Main Level.unity to the build order
     - Start button loads Main Level.unity

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
