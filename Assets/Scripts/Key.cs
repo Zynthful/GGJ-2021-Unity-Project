b3f2db7e93bd4c860c85d0db0c49fcbf0057c756
DanielNeale <danielneale01@gmail.com>
2021-01-29 21:55:22 +0000

    key

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField]
+    private int keyID = 0;
+    [SerializeField]
+    private Sprite image = null;
+
+    private int GetID()
+    {
+        return keyID;
+    }
+
+    private Sprite getImage()
+    {
+        return image;
+    }
+}
