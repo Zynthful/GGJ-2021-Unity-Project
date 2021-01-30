cb5705a8e677c46cda39af727770a453bff2ab79
lr228874 <lr228874@falmouth.ac.uk>
2021-01-30 13:20:22 +0000

    Started working on pause menu.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool paused = false;
+    [SerializeField] private GameObject PausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            ReceivedInput();
+        }
+    }
+
+    private void ReceivedInput() 
+    {
+        paused = !paused;
+        if (paused) Pause();
+        else Unpause();
+    }
+
+    private void Pause() 
+    {
+        PausePanel.SetActive(true);
+    }
+
+    private void Unpause() 
+    {
+        PausePanel.SetActive(false);
+    }
+}
