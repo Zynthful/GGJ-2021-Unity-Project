bc6470fa57efb5a5d1640739fed230029556cc03
DanielNeale <danielneale01@gmail.com>
2021-01-30 13:21:21 +0000

    The door script I forgot to commit

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private float swingForce = 1;
+
+    public void Unlock()
+    {
+        GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezeRotationY;
+
+        GetComponent<Rigidbody>().AddTorque(transform.up * swingForce, ForceMode.VelocityChange);
+    }
+}
