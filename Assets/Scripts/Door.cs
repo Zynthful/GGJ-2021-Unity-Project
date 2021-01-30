a9611799c0a8be7cf6c663202797cf51ab2e1df9
DanielNeale <danielneale01@gmail.com>
2021-01-30 14:11:50 +0000

    Doors now can be locked and unlocked

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,10 +7,20 @@ public class Door : MonoBehaviour
     [SerializeField]
     private float swingForce = 1;
 
+    private void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = Vector3.zero;
+    }
+
     public void Unlock()
     {
         GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezeRotationY;
 
         GetComponent<Rigidbody>().AddTorque(transform.up * swingForce, ForceMode.VelocityChange);
+    }
+
+    private void LateUpdate()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
     }
 }
