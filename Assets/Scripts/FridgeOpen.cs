34838849ea7b566477f4cc3ecef7d7cba25f30d9
DanielNeale <danielneale01@gmail.com>
2021-01-31 15:32:55 +0000

    Fridge is a fridge

---
+++ Assets/Scripts/FridgeOpen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FridgeOpen : MonoBehaviour
+{
+    private bool openDoor = false;
+    [SerializeField]
+    private int maxAngle = 50;
+    [SerializeField]
+    private float speed = -1;
+
+
+    void Update()
+    {
+        if (openDoor)
+        {
+            transform.Rotate(Vector3.up * speed);
+
+            if (transform.rotation.eulerAngles.y < maxAngle)
+            {
+                SceneManager.LoadScene(3);
+            }
+        }      
+    }
+
+
+    public void Open()
+    {
+        openDoor = true;
+    }
+}
--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -27,17 +27,21 @@ public class Lock : MonoBehaviour
 
     public void Unlock()
     {
-        connectedDoor.GetComponent<Door>().Unlock();
+        if (fridgeLock)
+        {
+            transform.parent.parent.GetChild(1).gameObject.AddComponent<Rigidbody>();
+            transform.parent.parent.GetChild(2).GetComponent<FridgeOpen>().Open();
+        }
+
+        else
+        {
+            connectedDoor.GetComponent<Door>().Unlock();            
+        }
 
         for (int i = 0; i < 2; i++)
         {
             transform.parent.GetChild(0).gameObject.AddComponent<Rigidbody>();
             transform.parent.GetChild(0).parent = null;
-        }
-
-        if (fridgeLock)
-        {
-            SceneManager.LoadScene(3);
         }
     }
 }
