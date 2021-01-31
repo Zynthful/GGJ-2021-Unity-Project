34dd4204b6b63b8a4ee2d8278ff2039928b11a0b
JamieC <jc228467@falmouth.ac.uk>
2021-01-31 13:23:08 +0000

    Added Audio Slider functionality using VCAs:
     - AudioSlider.cs is attached to each slider and given a VCA to control the volume of
     - Each slider uses OnValueChanged() to set the selected VCA's volume using a dynamic float

---
+++ Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,44 @@
+﻿/// Attach this script to an audio slider
+
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine.UI;
+
+public class AudioSlider : MonoBehaviour
+{
+    [SerializeField] [Tooltip("Enter the name of the VCA from FMOD")]
+    private string nameOfVCA = "Master"; // Default to Master
+
+    private Slider volSlider = null;
+    private VCA vca;    // FMOD Volume Control
+
+    [SerializeField] private float defaultVol = 0.75f;
+    private float currentVol;
+
+    private void OnEnable()
+    {
+        // Assign variables
+        vca = RuntimeManager.GetVCA($"vca:/{nameOfVCA}");
+        volSlider = GetComponent<Slider>();
+
+        // Initialise volume
+        currentVol = PlayerPrefs.GetFloat(nameOfVCA, defaultVol);      // Retrieves volume prefs if they exist, otherwise defaulting to 0.75f
+        Debug.Log(currentVol);
+        vca.setVolume(currentVol);
+        volSlider.value = currentVol;
+        //Debug.Log(currentVol);
+    }
+
+    /// <summary>
+    /// Called by Volume Slider as a dynamic float
+    /// Sets the selected VCA's volume
+    /// </summary>
+    /// <param name="vol"></param>
+    public void SetVolume(float vol)
+    {
+        vca.setVolume(vol);
+        PlayerPrefs.SetFloat(nameOfVCA, vol);
+        PlayerPrefs.Save();
+    }
+}
