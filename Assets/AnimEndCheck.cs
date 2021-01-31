4385f11bc7e83117433f32885ab552394555abb8
Cole Gilbert <colegilbert@cwgsy.net>
2021-01-31 16:53:30 +0000

    Implemented end game animation

---
+++ Assets/AnimEndCheck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimEndCheck : MonoBehaviour
+{
+    public bool animationEnd;
+    void Update()
+    {
+        if(animationEnd)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
